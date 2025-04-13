3e11d18d83ef600781e04474e5c427e2fa575c48
TuyetNguyen15 <Yang2004tuyet@gmail.com>
2025-04-13 23:30:36 +0700

    change

--- SE2-frontend-development/SE2-frontend-development/project/project/src/main/java/se/midterm/project/repository/BookedRoomRepository.java
+++ SE2-frontend-development/SE2-frontend-development/project/project/src/main/java/se/midterm/project/repository/BookedRoomRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 public interface BookedRoomRepository extends JpaRepository<BookedRoom, Long> {
     List<BookedRoom> findByRoomIdAndCheckInDateLessThanEqualAndCheckOutDateGreaterThanEqual(
             Long roomId, LocalDate checkOut, LocalDate checkIn);
+
     int countByStatus(BookingStatus status);
 
     List<BookedRoom> findByUserId(Long userId);
